54e84b252f83f0435921cc48f70600b6387624c9
Kelly Vergara <kellyvergara12@gmail.com>
2023-06-07 10:37:03 -0400

    commit

---
+++ completeRegistration.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RealStateManagement
+{
+    /// <summary>
+    /// Interaction logic for completeRegistration.xaml
+    /// </summary>
+    public partial class completeRegistration : Window
+    {
+        public completeRegistration()
+        {
+            InitializeComponent();
+        }
+    }
+}
